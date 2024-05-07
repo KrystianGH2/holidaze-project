d9ecbc0cc94928a1b4decb7e9b25cb5a1330bde1
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-08 00:52:23 +0200

    Renders Users Venues

--- app/(components)/Venues/MyVenues.jsx
+++ app/(components)/Venues/MyVenues.jsx
@@ -3,14 +3,18 @@ import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { getAllVenueByProfile } from "@/lib/api";
 import { Button } from "@/components/ui/button";
+import UpdateVenue from "../Modal/UpdateVenue/UpdateVenue";
+import { useAuth } from "@/app/Context/AuthContext";
 
 export default function MyVenues() {
+  const { isVenueManager } = useAuth();
   const userDataFromCookie = Cookies.get("userData");
   const parsedUserData = userDataFromCookie
     ? JSON.parse(userDataFromCookie)
     : null;
   const userName = parsedUserData ? parsedUserData.name : null;
   const [venues, setVenues] = useState(null);
+  const [selectedVenueId, setSelectedVenueId] = useState(null);
 
   useEffect(() => {
     const myVenues = async () => {
@@ -20,7 +24,6 @@ export default function MyVenues() {
         }
         const res = await getAllVenueByProfile(userName);
         setVenues(res.data);
-        console.log(res.data);
       } catch (err) {
         console.log(err);
       }
@@ -28,6 +31,14 @@ export default function MyVenues() {
 
     myVenues();
   }, [userName]);
+
+  const handleEditClick = (venueId) => {
+    setSelectedVenueId(venueId);
+    const modal = document.getElementById("my_modal_2");
+    if (modal) {
+      modal.showModal();
+    }
+  };
 
   return (
     <>
@@ -37,12 +48,36 @@ export default function MyVenues() {
             venues.map((venue) => (
               <div key={venue.id}>
                 {venue.name}
-
-                <Button onClick={() => handleEditClick(venue.id)}>Edit</Button>
+                {isVenueManager ? (
+                  <Button
+                    value={selectedVenueId}
+                    venueid={selectedVenueId}
+                    onClick={() => handleEditClick(venue.id)}
+                  >
+                    Edit
+                  </Button>
+                ) : (
+                  <div
+                    className="tooltip tooltip-error"
+                    data-tip="Register as a Venue Manager to continue."
+                  >
+                    <button disabled={true} className="btn btn-error">
+                      Edit
+                    </button>
+                  </div>
+                )}
               </div>
             ))}
         </div>
       </section>
+
+      {selectedVenueId && (
+        <UpdateVenue
+          id={"my_modal_2"}
+          venueid={selectedVenueId}
+          venues={venues}
+        />
+      )}
     </>
   );
 }
