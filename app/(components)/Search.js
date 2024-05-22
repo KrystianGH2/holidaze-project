fc0cff88b94a9a8c5489d4ae8909735041389a1a
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-22 21:16:50 +0200

    Searches for venues

--- app/(components)/Search.js
+++ app/(components)/Search.js
@@ -1,12 +1,25 @@
 "use client"
-import {useState} from 'react'
+import { useState } from 'react'
 import { useRouter } from 'next/navigation'
 import { Input } from '@/components/ui/input'
 import { Button } from '@/components/ui/button'
+import { Calendar as CalendarIcon } from "lucide-react"
+import { Calendar } from "@/components/ui/calendar"
+import { cn } from "@/lib/utils"
+import { addDays, format } from "date-fns"
+import {
+  Popover,
+  PopoverContent,
+  PopoverTrigger,
+} from "@/components/ui/popover"
 
-export default function Search() {
+export default function Search({className}) {
     const router = useRouter()
     const [query, setQuery] = useState('')
+    const [date, setDate] = useState({
+    from: new Date(),
+    to: addDays(new Date(), 1),
+  })
 
    const handleOnSearch = async (e) => {
      e.preventDefault()
@@ -19,14 +32,64 @@ export default function Search() {
 
   return (
     <>
-    <form className='flex w-full justify-center items-center text-black'>
-    <Input type="text" value={query} onChange={(e) => setQuery(e.target.value)} />
-    <Button variant="secondary"
-     className='btn'
-     disabled={isInputEmpty()}
-     type="submit" onClick={handleOnSearch}>Search</Button>
-    </form>
+      <form className='flex w-full justify-center items-center text-black max-w-7xl lg:px-8'>
+        <section className='flex flex-row w-full justify-center items-center gap-1 p-2'>
 
+       
+        <div className='flex-1 max-w-xl'>
+          <Input placeholder="Search your destination..." 
+           type="text" value={query}
+            onChange={(e) => setQuery(e.target.value)} 
+            className="h-[45px] focus:outline-none"
+            
+            />
+        </div>
+
+          <div className={cn("grid gap-2 ", className)}>
+            <Popover>
+        <PopoverTrigger asChild>
+          <Button
+            id="date"
+            variant={"secondary"}
+            className={cn(
+              "max-w-[500px] w-full flex  justify-start text-left font-normal h-[45px]",
+              !date && "text-muted-foreground"
+            )}
+          >
+            <CalendarIcon className="mr-2 h-4 w-4" />
+            {date?.from ? (
+              date.to ? (
+                <>
+                  {format(date.from, "LLL dd, y")} -{" "}
+                  {format(date.to, "LLL dd, y")}
+                </>
+              ) : (
+                format(date.from, "LLL dd, y")
+              )
+            ) : (
+              <span>Pick a date</span>
+            )}
+          </Button>
+        </PopoverTrigger>
+        <PopoverContent className="w-auto p-0" align="start">
+          <Calendar
+            initialFocus
+            mode="range"
+            defaultMonth={date?.from}
+            selected={date}
+            onSelect={setDate}
+            numberOfMonths={2}
+          />
+        </PopoverContent>
+            </Popover>
+          </div>
+        <Button 
+          variant="secondary"
+          className='btn disabled:bg-white  w-full max-w-[300px] min-h-[45px] text-black'
+          disabled={isInputEmpty()}
+          type="submit" onClick={handleOnSearch}>Search</Button>
+ </section>
+      </form>
     </>
   )
 }
