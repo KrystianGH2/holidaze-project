f6c6fcc9525c45eaf0a46e499ac2b6e4d4c6b4bb
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-26 00:16:59 +0200

    Shows users listed venues with upcoming bookings

--- app/(components)/Venues/MyVenues.jsx
+++ app/(components)/Venues/MyVenues.jsx
@@ -1,4 +1,5 @@
 "use client";
+import React from "react"; // Import React at the top of your file
 import Cookies from "js-cookie";
 import { useEffect, useState } from "react";
 import { getAllVenueByProfile, getProfiles } from "@/lib/api";
@@ -6,11 +7,20 @@ import { Button } from "@/components/ui/button";
 import UpdateVenue from "../Modal/UpdateVenue/UpdateVenue";
 import useDeleteVenue from "../Modal/DeleteVenue/useDeleteVenue";
 import BookingDetails from "../BookingDetails";
+import {
+  Table,
+  TableBody,
+  TableCaption,
+  TableCell,
+  TableFooter,
+  TableHead,
+  TableHeader,
+  TableRow,
+} from "@/components/ui/table";
+
 export default function MyVenues() {
   const userDataFromCookie = Cookies.get("userData");
-  const userName = userDataFromCookie
-    ? JSON.parse(userDataFromCookie).name
-    : null;
+  const userName = userDataFromCookie ? JSON.parse(userDataFromCookie).name : null;
 
   const [venues, setVenues] = useState(null);
   const [selectedVenueId, setSelectedVenueId] = useState(null);
@@ -51,55 +61,72 @@ export default function MyVenues() {
 
   return (
     <>
-      <section className="flex w-full justify-center items-center ">
-        <div className="flex flex-col w-full max-w-[1220px]">
-          {venues &&
-            venues.map((venue) => (
-              <div key={venue.id}>
-                {venue.name} <p>Number of bookings: {venue.bookings.length}</p>
-                <div
-                  className="tooltip"
-                  data-tip={
-                    iVenueManager
-                      ? null
-                      : "Register as a Venue Manager to continue."
-                  }
-                >
-                  <Button
-                    value={selectedVenueId}
-                    venueid={selectedVenueId}
-                    onClick={() => handleEditClick(venue.id)}
-                    disabled={!iVenueManager}
-                  >
-                    Edit
-                  </Button>
-                </div>
-                <div
-                  className="tooltip"
-                  data-tip={
-                    iVenueManager
-                      ? null
-                      : "Register as a Venue Manager to continue."
-                  }
-                >
-                  <Button
-                    className="hover:bg-red-500"
-                    onClick={() => handleDelete(venue.id)}
-                    disabled={!iVenueManager}
-                  >
-                    Delete venue
-                  </Button>
-                </div>
-                {venue.bookings.length <= 0 ? (
-                  ""
-                ) : (
-                  <>
-                    <BookingDetails bookings={venue.bookings} />
-                  </>
+      <style jsx>{`
+        .no-border-bottom {
+          border-bottom: none;
+        }
+        .hover-transparent:hover {
+          background-color: transparent;
+        }
+      `}</style>
+
+      <section className="flex w-full justify-center items-center max-w-5xl">
+        <Table>
+          <TableCaption>A list of your recent invoices.</TableCaption>
+          <TableHeader>
+            <TableRow>
+              <TableHead>Title</TableHead>
+              <TableHead className="text-left">Bookings</TableHead>
+              <TableHead className="w-[100px] text-center">Edit</TableHead>
+              <TableHead className="text-center">Amount</TableHead>
+            </TableRow>
+          </TableHeader>
+          <TableBody>
+            {venues?.map((venue) => (
+              <React.Fragment key={venue.id}>
+                <TableRow className={`${venue.bookings.length > 0 ? 'no-border-bottom' : ''} hover-transparent`}>
+                  <TableCell className="font-medium">{venue.name}</TableCell>
+                  <TableCell className="flex justify-start">{venue.bookings.length}</TableCell>
+                  <TableCell>
+                    <div
+                      className="tooltip"
+                      data-tip={iVenueManager ? null : "Register as a Venue Manager to continue."}
+                    >
+                      <Button
+                        value={selectedVenueId}
+                        venueid={selectedVenueId}
+                        onClick={() => handleEditClick(venue.id)}
+                        disabled={!iVenueManager}
+                      >
+                        Edit
+                      </Button>
+                    </div>
+                  </TableCell>
+                  <TableCell className="flex justify-center">
+                    <div
+                      className="tooltip"
+                      data-tip={iVenueManager ? null : "Register as a Venue Manager to continue."}
+                    >
+                      <Button
+                        onClick={() => handleDelete(venue.id)}
+                        disabled={!iVenueManager}
+                      >
+                        Delete venue
+                      </Button>
+                    </div>
+                  </TableCell>
+                </TableRow>
+                {venue.bookings.length > 0 && (
+                  <TableRow className="no-border-bottom">
+                    <TableCell className="px-0" colSpan={4}>
+                      <BookingDetails bookings={venue.bookings} />
+                    </TableCell>
+                  </TableRow>
                 )}
-              </div>
+              </React.Fragment>
             ))}
-        </div>
+          </TableBody>
+        </Table>
       </section>
 
       {selectedVenueId && (
