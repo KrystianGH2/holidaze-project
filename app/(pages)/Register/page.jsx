ad8ad279861a11bd7ee8b78bda7a1c2f2d49cd4f
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-26 16:43:19 +0200

    Register page styling

--- app/(pages)/Register/page.jsx
+++ app/(pages)/Register/page.jsx
@@ -3,9 +3,11 @@ import Register from "@/app/(components)/Register";
 
 export default function page() {
   return (
-    <div>
-      Register Page
+    <main className="flex flex-col justify-center w-full items-center">
+      <div className="pb-10">
+        <h1 className="text-xl font-bold">Register</h1>
+      </div>
       <Register />
-    </div>
+    </main>
   );
 }
