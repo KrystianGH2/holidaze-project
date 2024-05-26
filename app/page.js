3c90d7802a2d360969e0799cb97e2155cc8da461
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-26 21:00:14 +0200

    renders venues on homepage

--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
-import Venues from "./(components)/Venues/Venues";
+import HomeProducts from "./(components)/HomeProducts";
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-      <Venues/>
+    <main className="flex min-h-screen flex-col items-center justify-start ">
+      <HomeProducts/>
     </main>
   );
 }
