ccf64a5cf110ddb5a5ea877a4eb7c7537e77bac7
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-05-26 14:19:31 +0200

    integrates leaflet map for styling purposes

--- app/(pages)/Venue/[id]/page.jsx
+++ app/(pages)/Venue/[id]/page.jsx
@@ -8,6 +8,10 @@ import { LuParkingSquare } from "react-icons/lu";
 import { MdOutlineEmojiFoodBeverage, MdPets } from "react-icons/md";
 import { Rating } from "@smastrom/react-rating";
 import "@smastrom/react-rating/style.css";
+import { Button } from "@/components/ui/button";
+import "leaflet/dist/leaflet.css";
+import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
+import L from "leaflet";
 
 export default function VenuePage({ params }) {
   const id = params.id;
@@ -33,9 +37,18 @@ export default function VenuePage({ params }) {
     fetchVenue();
   }, [id]);
 
+  const markerIcon = new L.Icon({
+    iconUrl: require("leaflet/dist/images/marker-icon.png"),
+    iconSize: [25, 41],
+    iconAnchor: [12, 41],
+    popupAnchor: [1, -34],
+    shadowUrl: require("leaflet/dist/images/marker-shadow.png"),
+    shadowSize: [41, 41],
+  });
+
   return (
     <>
-      <main className=" flex flex-col justify-center items-center w-full lg:px-8 px-6">
+      <main className="flex flex-col justify-center items-center w-full lg:px-8 px-6">
         <section className="flex flex-col justify-start w-full max-w-7xl xl:px-8 gap-5">
           <div className="text-sm breadcrumbs">
             <ul>
@@ -54,30 +67,27 @@ export default function VenuePage({ params }) {
           </div>
 
           {loading ? (
-            <>
-              <section className="flex w-full justify-center ">
-                <div className="flex  justify-start items-center w-full max-w-7xl">
-                  <div className="flex flex-col gap-4 w-[800px]">
-                    <div className="skeleton h-[400px] w-full"></div>
-                    <div className="skeleton h-8 w-28"></div>
-                    <div className="skeleton h-8 w-full"></div>
-                    <div className="skeleton h-4 w-full"></div>
-                    <div className="skeleton h-8 w-full"></div>
-                  </div>
+            <section className="flex w-full justify-center">
+              <div className="flex justify-start items-center w-full max-w-7xl">
+                <div className="flex flex-col gap-4 w-[800px]">
+                  <div className="skeleton h-[400px] w-full"></div>
+                  <div className="skeleton h-8 w-28"></div>
+                  <div className="skeleton h-8 w-full"></div>
+                  <div className="skeleton h-4 w-full"></div>
+                  <div className="skeleton h-8 w-full"></div>
                 </div>
-              </section>
-            </>
+              </div>
+            </section>
           ) : (
             <>
-              {" "}
               <div className="flex flex-col gap-3">
                 <h2 className="font-semibold text-xl tracking-wide">
                   {venue?.name
                     ? venue?.name.charAt(0).toUpperCase() + venue?.name.slice(1)
                     : ""}
-                </h2>{" "}
+                </h2>
                 <div className="flex flex-row justify-start items-center gap-2 font-light text-sm">
-                  <SlLocationPin />{" "}
+                  <SlLocationPin />
                   <p>
                     <span>
                       {venue?.location.city ? (
@@ -98,47 +108,77 @@ export default function VenuePage({ params }) {
                   </p>
                 </div>
               </div>
-              <div className="flex w-full justify-start items-start flex-col-reverse  gap-4 md:flex-row md:h-[500px]">
+              <div className="flex w-full justify-start items-start flex-col-reverse gap-4 md:flex-row md:h-[500px]">
                 <div className="flex w-full md:h-full max-w-[800px] h-[500px]">
                   {venue?.media && (
                     <img
-                      className="w-full  h-full object-cover"
+                      className="w-full h-full object-cover"
                       src={venue.media[0].url}
                       alt={venue.media[0].alt}
                     />
                   )}
                 </div>
 
-                <section className="flex w-full flex-col justify-center items-end md:max-w-[300px] p-4 rounded border md:gap-1">
-                  <div className="flex flex-col py-2 gap-1">
-                    <div className="flex flex-row gap-2">
-                      <Rating
-                        style={{ maxWidth: 80, paddingTop: "12px" }}
-                        value={rating}
-                      />
-                      <p className="p-1  bg-blue-500  rounded-r-lg rounded-t-md h-auto px-3">
-                        {venue.rating}
-                      </p>
-                    </div>
-                    <small className="font-light tracking-wide text-gray-400">
-                      Booked {venue._count.bookings} times
-                    </small>
-                  </div>
-                  <hr />
-                  <div className="flex justify-end items-center ">
-                    <div className="avatar">
-                      <div className="w-[40px] rounded-full">
-                        <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                <div className="flex flex-col fw-full gap-4 overflow-hidden">
+                  <section className="flex w-full flex-col justify-center items-end md:max-w-[300px] p-4 rounded border md:gap-1">
+                    <div className="flex flex-col py-2 gap-1">
+                      <div className="flex flex-col py-2 gap-1 items-end">
+                        <div className="flex flex-row gap-2">
+                          <Rating
+                            style={{ maxWidth: 80, paddingTop: "12px" }}
+                            value={rating}
+                          />
+                          <p className="p-1 bg-blue-500 rounded-r-lg rounded-t-md h-auto px-3">
+                            {venue.rating}
+                          </p>
+                        </div>
+                        <small className="font-light tracking-wide text-gray-400">
+                          Booked {venue._count.bookings} times
+                        </small>
+                      </div>
+                      <hr />
+                      <div className="flex justify-end items-center">
+                        <div className="avatar">
+                          <div className="w-[40px] rounded-full">
+                            <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                          </div>
+                        </div>
+                        <p className="text-[13px] pt-2 font-extralight text-right">
+                          “Fine room, great view, convenient location, nice bar
+                          and restaurant, friendly staff.”
+                        </p>
                       </div>
                     </div>
-                    <p className="text-[13px] pt-2 font-extralight text-right">
-                      “Fine room, great view, convenient location, nice bar and
-                      restaurant, friendly staff.”
-                    </p>
+                  </section>
+
+                  <div className="relative md:flex w-full  group overflow-hidden">
+                    <MapContainer
+                      className="opacity-40 relative"
+                      style={{ height: "287px", width: "100%" }}
+                      center={[59.9139, 10.7522]}
+                      zoom={8}
+                      scrollWheelZoom={false}
+                      zoomControl={false}
+                      dragging={false}
+                      doubleClickZoom={false}
+                    >
+                      <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
+
+                      <Marker position={[59.9139, 10.7522]} icon={markerIcon}>
+                        <Popup>{venue?.location?.address}</Popup>
+                      </Marker>
+                    </MapContainer>
+                    <Button
+                      variant="secondary"
+                      className="rounded-sm absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 z-10"
+                    >
+                      Show on map
+                    </Button>
                   </div>
-                </section>
+                </div>
               </div>
-              <div className=" flex flex-row gap-1 md:text-[14px] text-[13px] font-light">
+
+              <div className="flex flex-row gap-1 md:text-[14px] text-[13px] font-light">
                 <ul className="flex flex-row gap-2">
                   <li
                     className={`flex flex-row justify-center items-center gap-2 ${
@@ -193,7 +233,7 @@ export default function VenuePage({ params }) {
               <div className="max-w-[700px] flex flex-col gap-2">
                 <h3 className="font-semibold text-xl tracking-wide">
                   Property Description
-                </h3>{" "}
+                </h3>
                 <p className="font-light text-[15px]">{venue?.description}</p>
               </div>
               {venue && bookings && (
