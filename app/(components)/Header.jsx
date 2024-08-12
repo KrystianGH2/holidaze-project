23b867b5491b157e9e12ffb9be258b0aa7a4469e
Krystian Gere <112473660+KrystianGH2@users.noreply.github.com>
2024-08-12 22:37:36 +0200

    checks windows pathname

--- app/(components)/Header.jsx
+++ app/(components)/Header.jsx
@@ -1,12 +1,35 @@
-import React from "react";
+"use client";
+import { useState, useEffect } from "react";
 import Navbar from "./Navbar";
 import Search from "./Search";
 
 export default function Header() {
+  const [windowPathName, setWindowPathName] = useState("");
+
+  if (typeof window !== "undefined") {
+  }
+  useEffect(() => {
+    const handlePathChange = () => {
+      setWindowPathName(window.location.pathname);
+    };
+
+    // Listen for popstate events
+    window.addEventListener("popstate", handlePathChange);
+
+    // Cleanup event listener on component unmount
+    return () => {
+      window.removeEventListener("popstate", handlePathChange);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col items-center justify-center w-full">
       <Navbar />
-      <Search />
+      {windowPathName !== "/Login" && (
+        <div className="flex w-full justify-center items-center">
+          <Search />
+        </div>
+      )}
     </div>
   );
 }
